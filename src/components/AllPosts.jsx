38d08be817c720ce791b232afaa37ef02b932b0a
FrostyHakim <hakim.malkawi@gmail.com>
2022-04-23 00:48:14 +0300

    Return Button Changes

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -6,7 +6,7 @@ import Back from "./Back"
 import "../styles/all-posts.css"
 
 const AllPosts = props => {
-    const {content, setShowPost, setCurrentPostData, back, scrollPosition, setScrollPosition, setContent} = props
+    const {content, setShowPost, setCurrentPostData, back, scrollPosition, setScrollPosition, setContent, language} = props
     useEffect(()=>{restoreScrollPosition(scrollPosition)}, [])
 
     return <>   <main className="all-posts">
@@ -18,6 +18,6 @@ const AllPosts = props => {
                         <h1 className="post-title">{post.title}</h1>
                         <img className="post-image" src={post.image} alt="Post"/>
                     </div> ) ) }
-                    <Back back={back} class="all" reset={setContent} />
+                    <Back language={language} back={back} class="all" reset={setContent} />
                 </main>  </> }
 export default AllPosts
