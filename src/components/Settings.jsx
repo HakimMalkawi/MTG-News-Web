1a0bd2eeedabea12ac8d2ac6ba5c42594f29e6c0
FrostyHakim <hakim.malkawi@gmail.com>
2022-04-28 01:16:41 +0300

    Updating Styling

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -7,7 +7,7 @@ const Settings = props => {
     const hideSettings = () => {
         document.querySelector(".settings-container").classList.toggle("show-left")
         props.setOptions(prevOptions => ({ ...prevOptions, on: !prevOptions.on }) )
-        setTimeout(() => props.setOptions(prevOptions => ({ ...prevOptions, settings: !prevOptions.settings } ) ), 1500 ) }
+        setTimeout(() => props.setOptions(prevOptions => ({ ...prevOptions, settings: !prevOptions.settings } ) ), 500 ) }
 
     useEffect( () => {
         document.querySelector(".settings-container").classList.toggle("show-left")
@@ -16,7 +16,7 @@ const Settings = props => {
     return <>  <main className="settings-container">
                     <button className="menu" onClick={hideSettings}>{"<"}</button>
                     <ul className="settings-options">
-                        <li onClick={ () => props.setDarkMode(prevDarkMode => !prevDarkMode)} className="settings-options-color">Dark Mode</li>
+                        <li onClick={ () => props.setDarkMode(prevDarkMode => !prevDarkMode)} className="settings-options-color">Toggle Color Scheme</li>
                     </ul>
                 </main>  </> }
 
