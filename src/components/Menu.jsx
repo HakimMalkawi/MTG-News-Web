fd2cd07e2875f00bdee79fb90d4eb96cc722acbb
helpmithridates <hakim.malkawi@gmail.com>
2022-04-28 18:09:06 +0300

    Automating Menu Item Creation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
 import Settings from "./Settings"
 import "../styles/menu.css"
 
 const Menu = props => {
     const {setToggleMenu, setRenderApp, setDarkMode} = props
-    const [options, setOptions] = useState({ on: false, settings: false, privacy_policy: false })
+    const [menu, setMenu] = useState( { on: false, 
+                                        options: [ { show: false, name: "Settings" }, { show: false, name: "Privacy Policy" } ] })
 
-    useEffect( () => !options.on && document.querySelector(".menu-container").classList.add("show-right"), [options.on])
+    useEffect( () => !menu.on && document.querySelector(".menu-container").classList.add("show-right"), [menu.on])
 
     const hideMenu = () => {
         setRenderApp(prevRenderApp => !prevRenderApp)
         document.querySelector(".menu-container").classList.toggle("show-right")
         setTimeout( () =>setToggleMenu(prevToggle => !prevToggle), 500) }
 
-    return  <>  { !options.on &&
+    const selectMenuItem = targetIndex => {
+        setMenu(prevMenu => ({ ...prevMenu, options: prevMenu.options.map((option, index) => 
+                                                                index === targetIndex ? {...option, show: !option.show} : option ) }) )
+        document.querySelector(".menu-container").classList.toggle("show-right")    } 
+
+    const menuItems = menu.options.map( (option, index) => {
+        return <li onClick={() => selectMenuItem(index)} className="menu-item" key={nanoid()}>{option.name}</li> } ) 
+
+    return  <>  { !menu.on &&
                     <main className="menu-container">
                         <ul className="menu-toggle" onClick={hideMenu}><li></li><li></li></ul>
-                        <ul className="menu-content">
-                            <li onClick={ () => {   setOptions(prevOptions => ({ ...prevOptions, settings: !prevOptions.settings }) )
-                                                    document.querySelector(".menu-container").classList.toggle("show-right")    } } 
-                                className="menu-item">Settings</li>
-                            <li className="menu-item">Privacy Policy</li>
-                        </ul>
+                        <ul className="menu-content">{menuItems}</ul>
                     </main> } 
                     
-                { options.settings && <Settings setOptions={setOptions} setDarkMode={setDarkMode} /> }    </> }
+                { menu.options[0].show && <Settings id={0} setMenu={setMenu} setDarkMode={setDarkMode} /> }    </> }
 export default Menu
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,19 +3,21 @@ import "../styles/back.css"
 import "../styles/settings.css"
 
 const Settings = props => {
-    const {setOptions, setDarkMode} = props
+    const {setMenu, setDarkMode} = props
 
     useEffect( () => {
         document.querySelector(".settings-container").classList.toggle("show-left")
-        setTimeout( () => setOptions(prevOptions => ({ ...prevOptions, on: !prevOptions.on })), 500) }, [] )
+        setTimeout( () => setMenu(prevMenu => ({ ...prevMenu, on: !prevMenu.on })), 500) }, [] )
 
-    const hideSettings = () => {
-        setOptions(prevOptions => ({ ...prevOptions, on: !prevOptions.on }) )
+    const hideSettings = targetIndex => {
+        setMenu(prevMenu => ({ ...prevMenu, on: !prevMenu.on }) )
         document.querySelector(".settings-container").classList.toggle("show-left")
-        setTimeout(() => setOptions(prevOptions => ({ ...prevOptions, settings: !prevOptions.settings } ) ), 500 ) }
+        setTimeout(() => setMenu(prevMenu => ({ ...prevMenu, options: prevMenu.options.map((option, index) => {
+            return index === targetIndex ? {...option, show: !option.show} : option
+        }) } ) ), 500 ) }
 
     return <>  <main className="settings-container">
-                    <button className="menu" onClick={hideSettings}>{"<"}</button>
+                    <button className="menu" onClick={()=>hideSettings(props.id)}>{"<"}</button>
                     <ul className="settings-options">
                         <li onClick={ () => setDarkMode(prevDarkMode => !prevDarkMode)} className="settings-options-color">Toggle Color Scheme</li>
                     </ul>
