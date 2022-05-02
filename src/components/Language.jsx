3423d8b4ef15f7035a9c03a003255329426b36fc
FrostyHakim <hakim.malkawi@gmail.com>
2022-05-03 00:12:56 +0300

    Changing Language Switcher

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,8 +2,21 @@ import "../styles/language.css"
 
 const Language = props => {
     const {language, setLanguage, home, setHome, setContent, setCurrentPostData, setShowPost} = props
-    return <>   <button onClick={ () => {   setLanguage(prevLanguage => !prevLanguage)
-                                            if(!home) { setHome(true); setContent(null); setCurrentPostData(null); setShowPost(false)} } } 
-                        className={`language-switcher ${language ? "EN" : "RU"}`}>
-                </button>  </> }
+
+    const refresh = () => {
+        if(!home) {
+            setHome(true)
+            setContent(null)
+            setCurrentPostData(null)
+            setShowPost(false)  } }
+    
+    const handleClick = mainLanguage => {
+        if(language !== mainLanguage) {
+            setLanguage(mainLanguage)
+            refresh()   } }
+
+    return <>   <select className={`language-switcher ${language ? "EN" : "RU"}`}>
+                    <option onClick={() => handleClick(true)}>EN</option>
+                    <option onClick={() => handleClick(false)}>RU</option>
+                </select>   </> }
 export default Language
